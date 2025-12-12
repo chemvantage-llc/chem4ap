ec61682c3b131a7951841272a9ddfd504c75d89e
Charles Wight <chuck.wight@gmail.com>
2025-12-12 09:39:00 -0700

    Final migration completion: Java 21 LTS upgrade validation complete - no code changes required

---
+++ front-end/exercises/build/precache-manifest.3f7cc435ee1667c0e109735c29764c92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9107dd779bbd8c395983fa1912a4224",
+    "url": "/exercises/index.html"
+  },
+  {
+    "revision": "59307eae50dbaca92273",
+    "url": "/exercises/static/css/2.dbb74888.chunk.css"
+  },
+  {
+    "revision": "59307eae50dbaca92273",
+    "url": "/exercises/static/js/2.082b9255.chunk.js"
+  },
+  {
+    "revision": "47fc8ca8c992b4558b8f",
+    "url": "/exercises/static/js/main.cd7a592e.chunk.js"
+  },
+  {
+    "revision": "67289519f1b2651c0cee",
+    "url": "/exercises/static/js/runtime~main.5cccae4e.js"
+  }
+]);
